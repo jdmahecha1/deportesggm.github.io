5a4790e0ea5d6285455cf538a741f34670aa478b
jdmahecha1 <168943133+jdmahecha1@users.noreply.github.com>
2024-05-26 21:01:40 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
-document.addEventListener('DOMContentLoaded' , ()=>{
-    const switcherTheme = document.querySelector('.main_check');
-    const root = document.documentElement;
+document.addEventListener('DOMContentLoaded', () => {
+  const switcherTheme = document.querySelector('.main_check');
+  const root = document.documentElement;
 
 
 
-    switcherTheme.addEventListener('click' , toggleTheme);
-    
-    function toggleTheme(){
-        const setTheme = switcherTheme.checked ? 'dark' : 'light';
+  switcherTheme.addEventListener('click', toggleTheme);
 
-        root.setAttribute('data-tema' , setTheme);
-    }
+  function toggleTheme() {
+    const setTheme = switcherTheme.checked ? 'dark' : 'light';
+
+    root.setAttribute('data-tema', setTheme);
+  }
 
 })
 
 const toggleMenu = () => {
-    const navigation = document.querySelector(".navigation");
-  
-    const burgerMenu = document.querySelector(".menu-icon");
-    const src = burgerMenu.getAttribute("src");
-  
-    const isBurger = src === "assets/burger-menu.svg";
-    const iconName = isBurger ? "assets/close.svg" : "assets/burger-menu.svg";
-  
-    burgerMenu.setAttribute("src", iconName);
-  
-    if (!isBurger) {
-      navigation.classList.add("navigation--mobile--fadeout");
-      setTimeout(() => {
-        navigation.classList.toggle("navigation--mobile");
-      }, 300);
-    } else {
-      navigation.classList.remove("navigation--mobile--fadeout");
+  const navigation = document.querySelector(".navigation");
+
+  const burgerMenu = document.querySelector(".menu-icon");
+  const src = burgerMenu.getAttribute("src");
+
+  const isBurger = src === "assets/burger-menu.svg";
+  const iconName = isBurger ? "assets/close.svg" : "assets/burger-menu.svg";
+
+  burgerMenu.setAttribute("src", iconName);
+
+  if (!isBurger) {
+    navigation.classList.add("navigation--mobile--fadeout");
+    setTimeout(() => {
       navigation.classList.toggle("navigation--mobile");
-    }
-  };
-  
+    }, 300);
+  } else {
+    navigation.classList.remove("navigation--mobile--fadeout");
+    navigation.classList.toggle("navigation--mobile");
+  }
+};
