40d0445303e4c1d8ae4e9b2a457583589a766da6
jdmahecha1 <168943133+jdmahecha1@users.noreply.github.com>
2024-07-15 19:21:46 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -35,3 +35,21 @@ const toggleMenu = () => {
     navigation.classList.toggle("navigation--mobile");
   }
 };
+
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 100 ){
+    document.querySelector('.go-top-container')
+    .classList.add('show');
+ }else{
+  document.querySelector('.go-top-container')
+    .classList.remove('show');
+ }
+}
+
+document.querySelector('.go-top-container')
+.addEventListener('click' , () =>{
+  window.scrollTo({
+    top:0,
+    behavior: 'smooth'
+  });
+});
